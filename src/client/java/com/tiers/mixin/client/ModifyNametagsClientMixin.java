5085a6f2f0f9ce565e48c6efa7cacc7e6e192c4e
Flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2026-02-03 20:36:14 +0100

    Crash fix | Version 0.7.1+1.21.4 Full Changelog: https://github.com/Flavio6561/Tiers/wiki/Version-changelogs

--- src/client/java/com/tiers/mixin/client/ModifyNametagsClientMixin.java
+++ src/client/java/com/tiers/mixin/client/ModifyNametagsClientMixin.java
@@ -1,18 +1,20 @@
 package com.tiers.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.tiers.TiersClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntityRenderer.class)
-public class ModifyNametagsClientMixin {
-    @Inject(at = @At("TAIL"), method = "updateRenderState(Lnet/minecraft/client/network/AbstractClientPlayerEntity;Lnet/minecraft/client/render/entity/state/PlayerEntityRenderState;F)V")
-    public void modifyPlayerDisplayNameAtRender(AbstractClientPlayerEntity abstractClientPlayerEntity, PlayerEntityRenderState playerEntityRenderState, float f, CallbackInfo ci) {
-        playerEntityRenderState.displayName = TiersClient.toggleMod ? TiersClient.addGetPlayer(abstractClientPlayerEntity.getNameForScoreboard(), false).getFullName(playerEntityRenderState.displayName) : playerEntityRenderState.displayName;
+@Mixin(PlayerEntity.class)
+public abstract class ModifyNametagsClientMixin {
+    @Shadow
+    public abstract String getNameForScoreboard();
+
+    @ModifyReturnValue(at = @At("RETURN"), method = "getDisplayName")
+    private Text modifyDisplayName(Text original) {
+        return TiersClient.toggleMod ? TiersClient.addGetPlayer(getNameForScoreboard(), false).getFullName(original) : original;
     }
 }
